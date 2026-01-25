66119a99beff6be49209191a321e0937b0b73d5e
Raquib Alam <raquib.alam2023@gmail.com>
2026-01-25 01:33:34 -0800

    Beta feedback: Bill Not Found fix, tip 15/18/20%, manual tax editing, scanned tax auto-fill, Clear button, UI fixes

--- app.config.ts
+++ app.config.ts
@@ -25,7 +25,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         ios: {
             supportsTablet: true,
             bundleIdentifier,
-            buildNumber: '7',
+            buildNumber: '8',
             infoPlist: {
                 ITSAppUsesNonExemptEncryption: false,
             },
