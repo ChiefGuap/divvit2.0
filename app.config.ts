ed8779d5d3fa61d63d07e234b0ae747903290734
Raquib Alam <raquib.alam2023@gmail.com>
2026-06-02 00:35:59 -0700

    chore: increment iOS build number to 26

--- app.config.ts
+++ app.config.ts
@@ -25,7 +25,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         ios: {
             supportsTablet: true,
             bundleIdentifier,
-            buildNumber: '25',
+            buildNumber: '26',
             infoPlist: {
                 ITSAppUsesNonExemptEncryption: false,
                 NSCameraUsageDescription: 'Divvit needs camera access to take group photos after splitting a bill.',
